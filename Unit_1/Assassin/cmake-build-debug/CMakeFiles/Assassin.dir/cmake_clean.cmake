1c3a5418c390852b5ce9cdb1a3adab47e69e1957
Yoshi <rytaneja@gmail.com>
2023-09-10 20:26:43 -0700

    assassubg again

---
+++ Unit_1/Assassin/cmake-build-debug/CMakeFiles/Assassin.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Assassin.exe"
+  "Assassin.exe.manifest"
+  "Assassin.pdb"
+  "CMakeFiles/Assassin.dir/main.cpp.obj"
+  "CMakeFiles/Assassin.dir/main.cpp.obj.d"
+  "libAssassin.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assassin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
