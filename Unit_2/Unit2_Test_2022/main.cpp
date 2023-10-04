1787189ecd9662dad0151a4881dc8b96702f57c7
Daroshi11260 <rytaneja@gmail.com>
2023-10-04 16:13:35 -0700

    test

--- Unit_2/Unit2_Test_2022/main.cpp
+++ Unit_2/Unit2_Test_2022/main.cpp
@@ -11,37 +11,87 @@ void removeEvens(stack<int>& numstack);
 
 void assignTask(queue<string>& taskQueue, set<string>& currentTasks);
 
+void joinClubs(map<string, set<string>>& clubMap, string lonelyStudent, string friendlyStudent);
+
+
+
+void removeFirstElements(stack<queue<int>>& s);
+
+set<string> getMostVisitedCountries(map<string, set<string>>& travelMap);
+
 int main() {
     cout << "Hello, World!" << endl;
 
-    stack<int> zero;
-    for (int i = 5; i <= 15; i++){
-        zero.push(i);
+
+    //testing joinclubs
+    map<string, set<string>> clubMap;
+    clubMap["Chess"].insert("A");
+    clubMap["Chess"].insert("B");
+    clubMap["Chess"].insert("C");
+    clubMap["Drama"].insert("A");
+    clubMap["Drama"].insert("B");
+    clubMap["Golf"].insert("B");
+    clubMap["Golf"].insert("C");
+    joinClubs(clubMap, "D", "A");
+
+    //print out clubmap
+
+    for(auto iter = clubMap.begin(); iter != clubMap.end(); iter++){
+        string club = iter->first;
+        set<string> members = iter->second;
+        cout << club << ": ";
+        for(string student :members){
+            cout << student << " ";
+        }
+        cout << endl;
     }
-    removeEvens(zero);
-    while(!zero.empty()){
-        cout << zero.top() << endl;
-        zero.pop();
+
+    //testing removeFirstElements
+    stack<queue<int>> s;
+    queue<int> q1;
+    for (int i = 0; i < 5; i++) {
+        q1.push(i);
+    }
+    s.push(q1);
+
+    //print out stack
+
+    removeFirstElements(s);
+
+    while (!s.empty()){
+        queue<int> q = s.top();
+        while (!q.empty()) {
+            cout << q.front() << " ";
+            q.pop();
+        }
+        cout << endl;
+        s.pop();
     }
 
-    queue<string> tasks;
-    tasks.push("A");
-    tasks.push("B");
-    tasks.push("C");
-    tasks.push("D");
+    //testing getMostVisitedCountries
 
-    set<string> currentTasks;
-    currentTasks.insert("A");
-    currentTasks.insert("Y");
-    currentTasks.insert("X");
-    currentTasks.insert("B");
+    map<string, set<string>> travelMap;
+    travelMap["A"].insert("USA");
+    travelMap["A"].insert("Canada");
+    travelMap["A"].insert("Mexico");
 
-    assignTask(tasks, currentTasks);
+    travelMap["B"].insert("USA");
+    travelMap["B"].insert("Canada");
 
-    while(!tasks.empty()){
-        cout << tasks.front() << endl;
-        tasks.pop();
+
+    travelMap["C"].insert("USA");
+    //travelMap["C"].insert("Canada");
+    travelMap["C"].insert("Mexico");
+    travelMap["C"].insert("France");
+
+    set<string> freq = getMostVisitedCountries(travelMap);
+
+    for (string country : freq) {
+        cout << country << endl;
     }
+
+
+
 
     return 0;
 }
@@ -92,4 +142,60 @@ map<string, set<string>> getCorrectClubMap (map<string, set<string>>& origmap){
         }
     }
     return newmap;
+}
+
+void joinClubs(map<string, set<string>>& clubMap, string lonelyStudent, string friendlyStudent){
+    for (auto iter = clubMap.begin(); iter != clubMap.end(); iter++) {
+        string club = iter->first;
+        if (clubMap[club].count(friendlyStudent) == 1) {
+            clubMap[club].insert(lonelyStudent);
+        }
+    }
+}
+
+void removeFirstElements(stack<queue<int>>& s){
+    stack<queue<int>> temp;
+    while(!s.empty()){
+        queue<int> q = s.top();
+        q.pop();
+        if(!q.empty()) {
+            temp.push(q);
+        }
+        s.pop();
+    }
+
+    //push back onto s from temp
+    while(!temp.empty()) {
+        s.push(temp.top());
+        temp.pop();
+    }
+}
+
+set<string> getMostVisitedCountries(map<string, set<string>>& travelMap){
+    map<string, int> freq;
+    int max = 0;
+    for (auto iter = travelMap.begin(); iter != travelMap.end(); iter++) {
+        string person = iter->first;
+        set<string> countries = iter->second;
+        for (string country : countries) {
+            freq[country]++;
+            if (freq[country] > max) {
+                max = freq[country];
+            }
+        }
+    }
+
+    //champ belt algo? no need, I did it above!
+    //int max = 0;
+
+    set<string> champBelt;
+    for (auto iter = freq.begin(); iter != freq.end(); iter++) {
+        string country = iter->first;
+        int count = iter->second;
+        if (count == max) {
+            champBelt.insert(country);
+        }
+    }
+
+    return champBelt;
 }
