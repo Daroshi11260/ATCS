9cf527e0c73eabd60c761556e3e41e2beed54fdb
Daroshi11260 <rytaneja@gmail.com>
2023-10-17 12:14:09 -0700

    test prep

--- Unit_3/Unit3Test2022/main.cpp
+++ Unit_3/Unit3Test2022/main.cpp
@@ -7,25 +7,68 @@ struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
+    TreeNode(){left=nullptr;right=nullptr;}
 };
 
 int countFavouriteNumbers(TreeNode *root, set<int> &favouriteNumbers);
 
+bool hasPathSum(TreeNode* root, int sum);
+
+int getLevelSumDifference(TreeNode* root);
+
 int main() {
     cout << "Hello, World!" << endl;
+
+    set<int> nums = {1, 2, 3, 4, 5};
+
+    TreeNode* root = new TreeNode();
+    root->val = 27;
+    root->left = new TreeNode();
+    root->left->val = 2;
+    root->right = new TreeNode();
+    root->right->val = 3;
+
+    cout << countFavouriteNumbers(root, nums) << endl;
+
+    if (hasPathSum(root, 30)) {
+        cout << "Has path sum" << endl;
+    } else {
+        cout << "Does not have path sum" << endl;
+    }
+
+    cout << getLevelSumDifference(root) << endl;
+
     return 0;
 }
 
-int countFavouriteNumbers(TreeNode *root, set<int> &favouriteNumbers) {
+int countFavouriteNumbers(TreeNode* root, set<int> &favouriteNumbers) {
     if (root == nullptr) {
         return 0;
     }
-    if (favouriteNumbers.count(root->val) == 1) {
-        return 1 + countFavouriteNumbers(root->left, favouriteNumbers)
-                 + countFavouriteNumbers(root->right, favouriteNumbers);
-    }
-
-    return 0 + countFavouriteNumbers(root->left, favouriteNumbers) 
-             + countFavouriteNumbers(root->right, favouriteNumbers);
+    return favouriteNumbers.count(root->val)
+            + countFavouriteNumbers(root->left, favouriteNumbers)
+            + countFavouriteNumbers(root->right, favouriteNumbers);
 }
 
+bool hasPathSum(TreeNode* root, int sum){
+    return root == nullptr ? sum == 0 : hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+}
+
+int levelSumHelper(TreeNode* root, int even, int odd){
+    if (root == nullptr) {
+        return 0;
+    }
+    return root->val - levelSumHelper(root->left, odd, even) - levelSumHelper(root->right, odd, even);
+}
+
+int getLevelSumDifference(TreeNode* root){
+
+    if (root == nullptr) {
+        return 0;
+    }
+    if (root == nullptr) {
+        return 0;
+    }
+    return root->val - getLevelSumDifference(root->left) - getLevelSum(root->right);
+
+}
